5a5ada13e3cba0415943106bcde1fe71eb593698
Breno Vergilio <bjvergilio47@gmail.com>
2022-08-09 10:11:43 -0400

    ✨ filling create-user-input with validations

--- src/user/dto/create-user.input.ts
+++ src/user/dto/create-user.input.ts
@@ -1,7 +1,33 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType } from '@nestjs/graphql';
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @IsString()
+  @MinLength(3, { message: 'Name minimum length is 3' })
+  @MaxLength(50, { message: 'Name maximum length is 50' })
+  //eslint-disable-next-line
+  @Matches('^[a-zA-ZÀ-ÿ\s\u00f1\u00d1]*$')
+  name: string;
+
+  @IsString()
+  //eslint-disable-next-line
+  @Matches('^\d{3}.\d{3}.\d{3}-\d{2}$')
+  cpf: string;
+
+  @IsEmail()
+  @MinLength(5, { message: 'Email minimum length is 5' })
+  @MaxLength(50, { message: 'Email maximum length is 50' })
+  email: string;
+
+  @IsString()
+  @MinLength(6, { message: 'Password minimum length is 6' })
+  @MaxLength(30, { message: 'Password maximum length is 30' })
+  password: string;
 }
